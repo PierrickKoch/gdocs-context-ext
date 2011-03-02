cbc6211989ff71f45ccc7ee1911f13bf2c4fb456
pierriko <pierrick.koch@gmail.com>
2011-03-02 11:48:25 +0700

    readme

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
-var DEFAULT_DOCS = "odt,doc,docx,pdf,pps,ppt,tif,tiff,xls,xlsx,pptx,pages,ai,psd,dxf,svg,eps,ps,xps";
+var DEFAULT_DOCS = "ai,doc,docx,dxf,eps,fnt,fon,odt,otf,pages,pdf,pps,ppt,pptx,ps,psd,svg,tif,tiff,ttf,xls,xlsx,xps";
 var SEPARATOR = ",";//"|";
 function isFirstRun() {
   return localStorage["docext"] == undefined;
@@ -17,7 +17,7 @@ function getTypesList() {
 }
 // handle mistyping and store the options
 function setTypes(types) {
-  var tmp = types.toLowerCase().trim().replace(/ /g,SEPARATOR).replace(/;/g,SEPARATOR);
+  var tmp = types.trim().replace(/ /g,SEPARATOR).replace(/;/g,SEPARATOR);
   while (tmp.indexOf(",,")!=-1) {
     tmp = tmp.replace(/,,/g,SEPARATOR);
   }
@@ -28,6 +28,15 @@ function setTypes(types) {
   if (tmp.lastIndexOf(SEPARATOR) == tmp.length-1) {
     tmp = tmp.substr(0,tmp.length-1);
   }
-  localStorage["docext"] = tmp;
-  return tmp;
+  uglylist = tmp.toLowerCase().split(SEPARATOR);
+  // remove duplicates
+  exts = new Array();
+  for (i = 0; i < uglylist.length; i++) {
+    if (exts.indexOf(uglylist[i]) == -1) {
+      exts.push(uglylist[i])
+    }
+  }
+  localStorage["docext"] = exts.sort().join(SEPARATOR);
+  return localStorage["docext"];
 }
+
